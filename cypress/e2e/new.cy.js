41041fcf278c3ffc7b4570260cb0ea5162bee189
MalachyCode <nwaformalachy2@gmail.com>
2023-12-05 20:52:40 +0100

    End to end testing implemented in workflow

---
+++ cypress/e2e/new.cy.js
@@ -0,0 +1,17 @@
+describe('Pokedex', function () {
+  beforeEach(function () {
+    cy.visit('http://localhost:5000');
+  });
+  it('front page can be opened', function () {
+    // cy.visit('http://localhost:5000');
+    cy.contains('ivysaur');
+    cy.contains(
+      'Pokémon and Pokémon character names are trademarks of Nintendo.'
+    );
+  });
+  it('can navigate from main page to a particular pokemon page', function () {
+    // cy.visit('http://localhost:5000');
+    cy.contains('ivysaur').click();
+    cy.contains('chlorophyll');
+  });
+});
